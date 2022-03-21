7599db4a47f77b66cb4f8f15d2171b515306fc4a
Sebastian-Neufeld <94544803+Sebastian-Neufeld@users.noreply.github.com>
2022-03-21 14:11:09 +0100

    more functions

--- SubstituteBadFrames.m
+++ SubstituteBadFrames.m
@@ -41,11 +41,22 @@ while i <= length(badFrames)
         % stack Dif is frame 53-3 = 50 up to 53+1 = 54 and 50 and 54 are
         % ok if 51,52,53 are bad - So we can calculate meaningful
         % "intermediate frames"
-        stackDiff = stack(:,:,badFrames(i) + 1) - ...
-                    stack(:,:,badFrames(i) - contBF);
+        % Conversion to int is important as the matrices are uint and a 
+        % difference between 2 uints will always be positive (absolute).
+        % Range uin16 = [ 0 2^16-1] (65535), int16 would only have [0 2^15-1] (32767).
+        % As no physiological value should be in that range we could "only"
+        % use int16 for the conversion, but then the whole stack should be
+        % saved as int16 and not only this step. This can be decided
+        % another day :D
+        stackDiff = int32(stack(:,:,badFrames(i) + 1)) - ...
+                    int32(stack(:,:,badFrames(i) - contBF)); 
         for f = 1:contBF
+            % Converting from to int32 is necessary to calculate the
+            % stack+stackDiff. then reverting back to uint16 ensures we
+            % still have the same datatype in the stack.
+            % (contBF+1 is important as for a single bad frame f = 1 contBF = 1 --> the whole dif is added instead of half, with f = 1 and f = 2 for contBF = 2 it will be 1/(2+1) and 2(/(2+1) so 1/3 and 2/3.
             stack(:,:,badFrames(i) - contBF + f) = ...
-                stack(:,:,badFrames(i) - contBF) + stackDiff*f/contBF;
+                uint16(int32(stack(:,:,badFrames(i) - contBF)) + stackDiff*f/(contBF+1)); 
         end
         contBF = 1;
     end
