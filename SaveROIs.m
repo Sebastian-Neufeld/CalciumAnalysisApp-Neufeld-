7558ce8beba07e0164e58391785cfff81b926612
Sebastian-Neufeld <94544803+Sebastian-Neufeld@users.noreply.github.com>
2022-03-23 20:55:51 +0100

    more functionality
    -Background is automatically assigned (though users can still edit the value more convenient now)
    -Distance is also displayed in µm
    -ROI drawing works without using ActiveContour first (struct is now "properly" defined) --- Also the program handels deleting all ROIs properly now
    - Traces as well as differently fitted dfF0 traces can now be exported to CSV (watch out, "." is used as decimal point which is conflicting to some language packs, e.g. German (where "," is the common decimalpoint!)

--- SaveROIs.m
+++ SaveROIs.m
@@ -3,14 +3,20 @@ function SaveROIs(dataPath, fileName, ROIsData)
 
 ROIsPath = [dataPath, fileName, '_ROIsData.mat'];
 
-% Just to make sure the already used ROI-characteristics are correct
-rp = regionprops(ROIsData.ROIs,'Area');
-ROIsData.sizes = [rp.Area];
-ROIsData.nROIs = length(ROIsData.sizes);
-%Now we need to take care what happens with traces / fits when ROIs are
-%deleted later on?
-    
-save(ROIsPath, 'ROIsData')
+% So if the user deltes all ROIs there won´t be any remnants of the struct
+% which could interfere with some functions/Checks
+if ROIsData.nROIs == 0
+    delete(ROIsPath)
+else
+    % Just to make sure the already used ROI-characteristics are correct
+    rp = regionprops(ROIsData.ROIs,'Area');
+    ROIsData.sizes = [rp.Area];
+    ROIsData.nROIs = length(ROIsData.sizes);
+    %Now we need to take care what happens with traces / fits when ROIs are
+    %deleted later on?
+
+    save(ROIsPath, 'ROIsData')
+end
     
 
     
