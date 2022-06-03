e19987acd2343dd9adf6507791381750c03b6659
Sebastian-Neufeld <94544803+Sebastian-Neufeld@users.noreply.github.com>
2022-06-03 10:52:03 +0200

    Clean up

--- test.m
+++
@@ -1,9 +0,0 @@
-x =(1:app.stackInfo.nFrames)';
-y = app.ROIsData.traces(:,2);
-modelfun = @(b,x) b(1) + b(2) * exp(b(3)-b(4)*x(:,1));
-beta0 = [100, 250, -0.5, -0.005];
-mdl = fitnlm(x, y, modelfun, beta0);
-
-coef = mdl.Coefficients{:, 'Estimate'};
-yFitted = coef(1) + coef(2) * exp(coef(3)-coef(4)*x);
-plot(yFitted)
